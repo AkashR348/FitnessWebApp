5e782956013690a1ad65760ab13ddf6ea72b3665
DrewPap <146133816+DrewPap@users.noreply.github.com>
2024-11-20 17:53:16 -0600

    Updated profile
    Profile page should be working now

--- FitnessApp/FitnessApp/settings.py
+++ FitnessApp/FitnessApp/settings.py
@@ -12,8 +12,6 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 
 from pathlib import Path
 
-from django.conf.global_settings import LOGOUT_REDIRECT_URL
-
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -133,4 +131,5 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-LOGIN_URL = '/login/'
+LOGIN_URL = '/'
+LOGOUT_REDIRECT_URL='/'
